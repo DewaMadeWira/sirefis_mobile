3f2e628d12300a0006e25578e71527b2c8e4970d
Dewa made <108676698+DewaMadeWira@users.noreply.github.com>
2023-10-01 05:59:39 +0700

    init

---
+++ lib/home.dart
@@ -0,0 +1,150 @@
+import 'package:flutter/material.dart';
+import 'package:sirefis_mobile/theme/colors.dart';
+import 'package:google_fonts/google_fonts.dart';
+
+class Home extends StatefulWidget {
+  const Home({super.key});
+
+  @override
+  State<Home> createState() => _HomeState();
+}
+
+class _HomeState extends State<Home> {
+  String dropdownPerusahaan = "NVIDIA";
+  List<String> perusahaanList = ["NVIDIA", "AMD", "Apple"];
+
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: ElevatedButton(
+          style: ElevatedButton.styleFrom(
+              backgroundColor: primaryColor,
+              foregroundColor: Colors.black,
+              elevation: 0,
+              shape: RoundedRectangleBorder(
+                  side: const BorderSide(
+                      width: 1, // thickness
+                      color: Colors.black // color
+                      ),
+                  borderRadius: BorderRadius.circular(8))),
+          onPressed: () {
+            showModalBottomSheet(
+                shape: const RoundedRectangleBorder(
+                  borderRadius: BorderRadius.vertical(
+                    top: Radius.circular(10.0),
+                  ),
+                ),
+                context: context,
+                builder: (BuildContext context) {
+                  return Padding(
+                    padding: const EdgeInsets.all(20.0),
+                    child: Wrap(children: [
+                      Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          Text(
+                            "Pilih Kriteria Rekomendasi",
+                            style: GoogleFonts.jura(
+                                fontSize: 20, fontWeight: FontWeight.bold),
+                          ),
+                          SizedBox(
+                            height: 20,
+                          ),
+                          Text(
+                            "Tertinggi",
+                            style: GoogleFonts.jura(
+                                fontSize: 16, fontWeight: FontWeight.bold),
+                          ),
+                          SizedBox(
+                            height: 10,
+                          ),
+                          Row(
+                            mainAxisAlignment: MainAxisAlignment.spaceAround,
+                            children: [
+                              BtnFilter(text: "Terendah"),
+                              BtnFilter(text: "Tertinggi"),
+                              BtnFilter(text: "Termahal"),
+                              BtnFilter(text: 'Termurah')
+                            ],
+                          ),
+                          SizedBox(
+                            height: 20,
+                          ),
+                          Text(
+                            "Perusahaan",
+                            style: GoogleFonts.jura(
+                                fontSize: 16, fontWeight: FontWeight.bold),
+                          ),
+                          SizedBox(
+                            height: 10,
+                          ),
+                          DropdownButton(
+                            dropdownColor: backgroundColor,
+                            style: GoogleFonts.inter(
+                                fontSize: 14, color: Colors.black),
+                            // padding: EdgeInsets.all(1),
+                            items: perusahaanList.map((String items) {
+                              return DropdownMenuItem(
+                                value: items,
+                                child: Text(items),
+                              );
+                            }).toList(),
+                            onChanged: (String? newValue) {
+                              setState(() {
+                                dropdownPerusahaan = newValue!;
+                              });
+                            },
+                            value: dropdownPerusahaan,
+                            // icon: Icon(Icons.arrow_drop_down),
+                          ),
+                          // BtnFilter(),
+                          Text(
+                            "Tipe",
+                            style: GoogleFonts.jura(
+                                fontSize: 16, fontWeight: FontWeight.bold),
+                          ),
+                          Text(
+                            "Rentang Harga",
+                            style: GoogleFonts.jura(
+                                fontSize: 16, fontWeight: FontWeight.bold),
+                          ),
+                          Text(
+                            "Tahun",
+                            style: GoogleFonts.jura(
+                                fontSize: 16, fontWeight: FontWeight.bold),
+                          ),
+                        ],
+                      ),
+                    ]),
+                  );
+                });
+          },
+          child: Text("Click me")),
+    );
+  }
+}
+
+class BtnFilter extends StatelessWidget {
+  final String text;
+  const BtnFilter({super.key, required this.text});
+
+  @override
+  Widget build(BuildContext context) {
+    return ElevatedButton(
+      child: Text(
+        text,
+        style: GoogleFonts.inter(
+          fontSize: 10,
+        ),
+      ),
+      onPressed: () {},
+      style: ElevatedButton.styleFrom(
+          padding: EdgeInsets.all(0),
+          backgroundColor: backgroundColor,
+          foregroundColor: Colors.black,
+          elevation: 1,
+          shape:
+              RoundedRectangleBorder(borderRadius: BorderRadius.circular(5))),
+    );
+  }
+}
---
+++ lib/theme/colors.dart
@@ -0,0 +1,5 @@
+import 'package:flutter/material.dart';
+
+var primaryColor = Color(0xFF52D766);
+var backgroundColor = Color(0xFFF2F2F2);
+var whiteColor = Color(0xFFFFFDFD);
