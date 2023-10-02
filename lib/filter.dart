7ca11b4bcdf771b3749291b3c8e8f64694f97cf4
Dewa made <108676698+DewaMadeWira@users.noreply.github.com>
2023-10-02 21:01:02 +0700

    add api call

--- lib/filter.dart
+++ lib/filter.dart
@@ -1,7 +1,10 @@
+import 'dart:convert';
+
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
 import 'package:sirefis_mobile/components/dropDownFilter.dart';
 import 'package:sirefis_mobile/theme/colors.dart';
+import 'package:http/http.dart' as http;
 
 import 'model/item.dart';
 
@@ -30,118 +33,147 @@ class _FilterPageState extends State<FilterPage> {
     });
   }
 
+  List<Item> items = [];
+
+  void textme() {
+    print("this is text");
+  }
+
+  Future getGpu() async {
+    var res = await http.get(Uri.http("192.168.1.11:8080", "gpu"));
+    var jsonData = jsonDecode(res.body);
+    if (items.isNotEmpty) {
+      items.clear();
+      print("it is empty");
+    }
+    for (var itemData in jsonData) {
+      final item = Item(
+          name: itemData["gpu_name"], price: itemData["gpu_price"].toString());
+      items.add(item);
+    }
+
+    print(items.length);
+  }
+
   @override
   Widget build(BuildContext context) {
+    textme();
+    getGpu();
+    // return FutureBuilder(builder: builder)
     return Scaffold(
       backgroundColor: backgroundColor,
       appBar: AppBar(
         backgroundColor: primaryColor,
         elevation: 0,
       ),
-      body: Container(
-        padding: EdgeInsets.all(25),
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            Text(
-              "Hasil \nRekomendasi",
-              style: GoogleFonts.jura(fontSize: 36),
-            ),
-            SizedBox(
-              height: 20,
-            ),
-            Text(
-              "Urutkan :",
-              style: GoogleFonts.jura(fontSize: 20),
-            ),
-            SizedBox(
-              height: 20,
-            ),
-            DropDownFilter(
-                item: dropdownTipe, itemList: tipeList, onChange: setTipe),
-            SizedBox(
-              height: 20,
-            ),
-            Column(
-              mainAxisAlignment: MainAxisAlignment.spaceAround,
-              children: [
-                ListView.builder(
-                    shrinkWrap: true,
-                    itemCount: itemList.length,
-                    itemBuilder: (context, index) {
-                      return Column(
-                        children: [
-                          Container(
-                            padding: EdgeInsets.all(15),
-                            decoration: BoxDecoration(
-                                boxShadow: [
-                                  BoxShadow(
-                                    color: Colors.grey,
-                                    spreadRadius: 2,
-                                    blurRadius: 7,
-                                    offset: Offset(0, 2),
-                                  )
-                                ],
-                                color: whiteColor,
-                                border:
-                                    Border.all(color: Colors.black, width: 2),
-                                borderRadius: BorderRadius.circular(13)),
-                            child: Row(
-                              mainAxisAlignment: MainAxisAlignment.start,
-                              crossAxisAlignment: CrossAxisAlignment.center,
-                              children: [
-                                Text(
-                                  'Gambar\nTidak\nTersedia',
-                                  textAlign: TextAlign.center,
-                                  style: GoogleFonts.inter(
-                                      fontSize: 13,
-                                      fontWeight: FontWeight.bold,
-                                      color: Colors.grey),
-                                ),
-                                SizedBox(
-                                  width: 50,
-                                ),
-                                Column(
-                                  mainAxisSize: MainAxisSize.max,
-                                  mainAxisAlignment:
-                                      MainAxisAlignment.spaceBetween,
-                                  crossAxisAlignment: CrossAxisAlignment.start,
-                                  children: [
-                                    Container(
-                                      width: 150,
-                                      child: Text(
-                                        itemList[index].name,
-                                        style: GoogleFonts.jura(
-                                          fontSize: 16,
-                                          fontWeight: FontWeight.bold,
-                                        ),
-                                      ),
-                                    ),
-                                    SizedBox(
-                                      height: 15,
-                                    ),
-                                    Text(
-                                      "Rp" + itemList[index].price,
-                                      style: GoogleFonts.inter(
-                                        fontSize: 14,
-                                      ),
+      body: ListView(children: [
+        Container(
+          padding: EdgeInsets.all(25),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Text(
+                "Hasil \nRekomendasi",
+                style: GoogleFonts.jura(fontSize: 36),
+              ),
+              SizedBox(
+                height: 20,
+              ),
+              Text(
+                "Urutkan :",
+                style: GoogleFonts.jura(fontSize: 20),
+              ),
+              SizedBox(
+                height: 20,
+              ),
+              DropDownFilter(
+                  item: dropdownTipe, itemList: tipeList, onChange: setTipe),
+              SizedBox(
+                height: 20,
+              ),
+              Column(
+                mainAxisAlignment: MainAxisAlignment.spaceAround,
+                children: [
+                  ListView.builder(
+                      physics: const NeverScrollableScrollPhysics(),
+                      shrinkWrap: true,
+                      itemCount: items.length,
+                      itemBuilder: (context, index) {
+                        return Column(
+                          children: [
+                            Container(
+                              padding: EdgeInsets.all(15),
+                              decoration: BoxDecoration(
+                                  boxShadow: [
+                                    BoxShadow(
+                                      color: Colors.grey,
+                                      spreadRadius: 2,
+                                      blurRadius: 7,
+                                      offset: Offset(0, 2),
                                     )
                                   ],
-                                )
-                              ],
+                                  color: whiteColor,
+                                  border:
+                                      Border.all(color: Colors.black, width: 2),
+                                  borderRadius: BorderRadius.circular(13)),
+                              child: Row(
+                                mainAxisAlignment: MainAxisAlignment.start,
+                                crossAxisAlignment: CrossAxisAlignment.center,
+                                children: [
+                                  Text(
+                                    'Gambar\nTidak\nTersedia',
+                                    textAlign: TextAlign.center,
+                                    style: GoogleFonts.inter(
+                                        fontSize: 13,
+                                        fontWeight: FontWeight.bold,
+                                        color: Colors.grey),
+                                  ),
+                                  SizedBox(
+                                    width: 50,
+                                  ),
+                                  Column(
+                                    mainAxisSize: MainAxisSize.max,
+                                    mainAxisAlignment:
+                                        MainAxisAlignment.spaceBetween,
+                                    crossAxisAlignment:
+                                        CrossAxisAlignment.start,
+                                    children: [
+                                      Container(
+                                        width: 150,
+                                        child: Text(
+                                          items[index].name,
+                                          style: GoogleFonts.jura(
+                                            fontSize: 16,
+                                            fontWeight: FontWeight.bold,
+                                          ),
+                                        ),
+                                      ),
+                                      SizedBox(
+                                        height: 15,
+                                      ),
+                                      Text(
+                                        "Rp" + items[index].price,
+                                        style: GoogleFonts.inter(
+                                          fontSize: 14,
+                                        ),
+                                      )
+                                    ],
+                                  )
+                                ],
+                              ),
                             ),
-                          ),
-                          SizedBox(
-                            height: 20,
-                          )
-                        ],
-                      );
-                    }),
-              ],
-            )
-          ],
+                            SizedBox(
+                              height: 20,
+                            )
+                          ],
+                        );
+                      }),
+                ],
+              )
+            ],
+          ),
         ),
-      ),
+      ]),
     );
   }
 }
