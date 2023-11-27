03542b994f9231b95f423bfafcc5cffb4e0def74
Dewa made <108676698+DewaMadeWira@users.noreply.github.com>
2023-11-27 08:05:10 +0700

    add loading state

--- lib/filter.dart
+++ lib/filter.dart
@@ -112,136 +112,136 @@ class _FilterPageState extends State<FilterPage> {
     getGpu();
     // return FutureBuilder(builder: builder)
     return FutureBuilder(
-      // future: getGpu(),
-      builder: (ctx, snapshot) {
-        // if (snapshot.connectionState == ConnectionState.done) {
-        return Scaffold(
-          backgroundColor: backgroundColor,
-          appBar: AppBar(
-            backgroundColor: primaryColor,
-            elevation: 0,
-          ),
-          body: ListView(children: [
-            Container(
-              padding: EdgeInsets.all(30),
-              child: Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: [
-                  Text(
-                    "Hasil \nRekomendasi",
-                    style: GoogleFonts.jura(fontSize: 36),
-                  ),
-                  SizedBox(
-                    height: 20,
-                  ),
-                  Text(
-                    "Urutkan :",
-                    style: GoogleFonts.jura(fontSize: 20),
-                  ),
-                  SizedBox(
-                    height: 20,
-                  ),
-                  DropDownFilter(
-                      item: dropdownTipe,
-                      itemList: tipeList,
-                      onChange: setTipe),
-                  SizedBox(
-                    height: 20,
-                  ),
-                  Column(
-                    mainAxisAlignment: MainAxisAlignment.spaceAround,
+        future: getGpu(),
+        builder: (ctx, snapshot) {
+          if (snapshot.connectionState == ConnectionState.done) {
+            return Scaffold(
+              backgroundColor: backgroundColor,
+              appBar: AppBar(
+                backgroundColor: primaryColor,
+                elevation: 0,
+              ),
+              body: ListView(children: [
+                Container(
+                  padding: EdgeInsets.all(25),
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
                     children: [
-                      ListView.builder(
-                          physics: const NeverScrollableScrollPhysics(),
-                          shrinkWrap: true,
-                          itemCount: itemList.length,
-                          itemBuilder: (context, index) {
-                            return Column(
-                              children: [
-                                Container(
-                                  padding: EdgeInsets.all(15),
-                                  decoration: BoxDecoration(
-                                      boxShadow: [
-                                        BoxShadow(
-                                          color: Colors.grey,
-                                          spreadRadius: 2,
-                                          blurRadius: 7,
-                                          offset: Offset(0, 2),
-                                        )
-                                      ],
-                                      color: whiteColor,
-                                      border: Border.all(
-                                          color: Colors.black, width: 2),
-                                      borderRadius: BorderRadius.circular(13)),
-                                  child: Row(
-                                    mainAxisAlignment: MainAxisAlignment.start,
-                                    crossAxisAlignment:
-                                        CrossAxisAlignment.center,
-                                    children: [
-                                      Text(
-                                        'Gambar\nTidak\nTersedia',
-                                        textAlign: TextAlign.center,
-                                        style: GoogleFonts.inter(
-                                            fontSize: 13,
-                                            fontWeight: FontWeight.bold,
-                                            color: Colors.grey),
-                                      ),
-                                      SizedBox(
-                                        width: 50,
-                                      ),
-                                      Column(
-                                        mainAxisSize: MainAxisSize.max,
+                      Text(
+                        "Hasil \nRekomendasi",
+                        style: GoogleFonts.jura(fontSize: 36),
+                      ),
+                      SizedBox(
+                        height: 20,
+                      ),
+                      Text(
+                        "Urutkan :",
+                        style: GoogleFonts.jura(fontSize: 20),
+                      ),
+                      SizedBox(
+                        height: 20,
+                      ),
+                      DropDownFilter(
+                          item: dropdownTipe,
+                          itemList: tipeList,
+                          onChange: setTipe),
+                      SizedBox(
+                        height: 20,
+                      ),
+                      Column(
+                        mainAxisAlignment: MainAxisAlignment.spaceAround,
+                        children: [
+                          ListView.builder(
+                              physics: const NeverScrollableScrollPhysics(),
+                              shrinkWrap: true,
+                              itemCount: items.length,
+                              itemBuilder: (context, index) {
+                                return Column(
+                                  children: [
+                                    Container(
+                                      padding: EdgeInsets.all(15),
+                                      decoration: BoxDecoration(
+                                          boxShadow: [
+                                            BoxShadow(
+                                              color: Colors.grey,
+                                              spreadRadius: 2,
+                                              blurRadius: 7,
+                                              offset: Offset(0, 2),
+                                            )
+                                          ],
+                                          color: whiteColor,
+                                          border: Border.all(
+                                              color: Colors.black, width: 2),
+                                          borderRadius:
+                                              BorderRadius.circular(13)),
+                                      child: Row(
                                         mainAxisAlignment:
-                                            MainAxisAlignment.spaceBetween,
+                                            MainAxisAlignment.start,
                                         crossAxisAlignment:
-                                            CrossAxisAlignment.start,
+                                            CrossAxisAlignment.center,
                                         children: [
-                                          Container(
-                                            width: 150,
-                                            child: Text(
-                                              itemList[index].name,
-                                              style: GoogleFonts.jura(
-                                                fontSize: 16,
+                                          Text(
+                                            'Gambar\nTidak\nTersedia',
+                                            textAlign: TextAlign.center,
+                                            style: GoogleFonts.inter(
+                                                fontSize: 13,
                                                 fontWeight: FontWeight.bold,
-                                              ),
-                                            ),
+                                                color: Colors.grey),
                                           ),
                                           SizedBox(
-                                            height: 15,
+                                            width: 50,
                                           ),
-                                          Text(
-                                            "Rp" + itemList[index].price,
-                                            style: GoogleFonts.inter(
-                                              fontSize: 14,
-                                            ),
+                                          Column(
+                                            mainAxisSize: MainAxisSize.max,
+                                            mainAxisAlignment:
+                                                MainAxisAlignment.spaceBetween,
+                                            crossAxisAlignment:
+                                                CrossAxisAlignment.start,
+                                            children: [
+                                              Container(
+                                                width: 150,
+                                                child: Text(
+                                                  items[index].name,
+                                                  style: GoogleFonts.jura(
+                                                    fontSize: 16,
+                                                    fontWeight: FontWeight.bold,
+                                                  ),
+                                                ),
+                                              ),
+                                              SizedBox(
+                                                height: 15,
+                                              ),
+                                              Text(
+                                                "Rp" + items[index].price,
+                                                style: GoogleFonts.inter(
+                                                  fontSize: 14,
+                                                ),
+                                              )
+                                            ],
                                           )
                                         ],
-                                      )
-                                    ],
-                                  ),
-                                ),
-                                SizedBox(
-                                  height: 20,
-                                )
-                              ],
-                            );
-                          }),
+                                      ),
+                                    ),
+                                    SizedBox(
+                                      height: 20,
+                                    )
+                                  ],
+                                );
+                              }),
+                        ],
+                      )
                     ],
-                  )
-                ],
-              ),
-            ),
-          ]),
-        );
-        // } else {
-        //   return Center(
-        //       child: CircularProgressIndicator(
-        //     backgroundColor: backgroundColor,
-        //     color: primaryColor,
-        //   ));
-        // }
-      },
-      future: null,
-    );
+                  ),
+                ),
+              ]),
+            );
+          } else {
+            return Center(
+                child: CircularProgressIndicator(
+              backgroundColor: backgroundColor,
+              color: primaryColor,
+            ));
+          }
+        });
   }
 }
--- lib/model/item.dart
+++ lib/model/item.dart
@@ -1,15 +1,9 @@
-class Item{
-  final String name;//gpu_name
-  final String price;//pirce gpu
-  
-  Item({
-    required this.name, 
-    required this.price
-  
-  });
+class Item {
+  final String name; //gpu_name
+  final String price; //pirce gpu
 
-  factory Item.fromJson(Map<String, dynamic>json)=> Item(
-    name: json['name'], 
-    price: json['price']
-  );
+  Item({required this.name, required this.price});
+
+  factory Item.fromJson(Map<String, dynamic> json) =>
+      Item(name: json['gpu_name'], price: json['price']);
 }
